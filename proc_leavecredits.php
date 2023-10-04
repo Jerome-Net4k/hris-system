a74f3c8c5173e20965611ba163369a02f527682d
net4k <kompanyero24@gmail.com>
2023-10-05 05:52:56 +0800

    modified:   proc_leavecredits.php

--- proc_leavecredits.php
+++ proc_leavecredits.php
@@ -81,11 +81,11 @@ function displayRecords($monthRecords){
         <td class="text-center">+ 1.25</td>
         <td class="text-center">+ 1.25</td>
         <td colspan="5"></td>
-        <td class="text-center" style="padding:4px"><button type="submit" class="btn btn-success " id="handleUpdateButton"><i class="fi fi-rr-edit"> Update</i></button></td>
+        <td class="text-center" style="padding:4px"><button type="submit" class="btn btn-success " id="region"><i class="fi fi-rr-edit"> Update</i></button></td>
         
         </tr>';
         echo'<script> 
-        $("button#btndel").on("click",function(){
+        $("button#fsdf").on("click",function(){
             var empNo = $(this).val();
             window.location.href="rnrdash.php?id=" + empNo;
             })
